8b63391d04e52f6a0765ad1528d7b16fbbe21294
Claudio Lugo <claudiomlugo@gmail.com>
2022-11-01 21:30:07 -0300

    retocando

---
+++ js/jsarticulos.js
@@ -0,0 +1,32 @@
+const articulos = [
+    {
+        id: 1,
+        nombre: `stanley\nblack`,
+        precio: 40000,
+        imagen: `stanleyblack`,
+    },
+    {
+        id: 2,
+        nombre: `stanley\nred`,
+        precio: 35000,
+        imagen: `stanleyred`,
+    },
+    {
+        id: 3,
+        nombre: `stanley\nwhite`,
+        precio: 30000,
+        imagen: `stanleywhite1`,
+    },
+    {
+        id: 4,
+        nombre: `stanley\npink`,
+        precio: 43000,
+        imagen: `stanleypink2`,
+    },
+    {
+        id: 5,
+        nombre: `combo\nstanley`,
+        precio: 90000,
+        imagen: `stanleycombo`,
+    }
+];
---
+++ js/jsprueba.js
@@ -0,0 +1,89 @@
+let carrito = [];
+let cards = document.getElementById("cards");
+let btnFC = document.getElementById("btnFC");
+
+function ajustararticulos() {
+    for (const producto of articulos) {
+        cards.innerHTML += `
+        <div class="card container-fluid col-sd-3">
+        <img src=./images/${producto.imagen}.jpg class="card-img-top" alt=${producto.imagen}>
+        <div class="card-body">
+            <h3 class="card-text">${producto.nombre.toUpperCase()}</h3>
+            <p class="card-text">$ ${producto.precio}</p>
+            <button id='btn${producto.id}' class="btn btn-ligth">Comprar</button>
+        </div>
+        </div>
+    `;
+    }
+    articulos.forEach((producto) => {
+        document
+            .getElementById(`btn${producto.id}`)
+            .addEventListener("click", function () {
+                agregarCarrito(producto);
+            });
+    });
+}
+ajustararticulos();
+
+function agregarCarrito(productoComprar) {
+    carrito.push(productoComprar);
+    const jsonStorage = (clave, valor) => { localStorage.setItem(clave, valor) };
+    jsonStorage(`listaArticulos`, JSON.stringify(productoComprar));
+    console.log(jsonStorage)
+    const storageJson = JSON.parse(localStorage.getItem(`listaArticulos`));
+    console.log(storageJson);
+    Swal.fire({
+        imageUrl: `./images/${productoComprar.imagen}.jpg`,
+        imageWidth: 250,
+        imageHeight: 250,
+        imageAlt: productoComprar.nombre,
+        title: productoComprar.nombre.toUpperCase(),
+        text: "Agregado a tu carro de compras!",
+        width: 400,
+        showConfirmButton: false,
+        timer: 1750,
+        background: `#f0e8e8`,
+        //color: ``,
+        showClass: {
+            popup: "animate__animated animate__jackInTheBox",
+        },
+        hideClass: {
+            popup: "animate__animated animate__zoomOutDown",
+        },
+    });
+    document.getElementById("tBody").innerHTML += `
+    <tr>
+        <td>${productoComprar.nombre.toUpperCase()}</td>
+        <td>${productoComprar.precio}</td>
+        <td><button id='btn${productoComprar.id
+        }' class="btn btn-ligth"><i class="fa-solid fa-trash-can"></i></button></td>
+    </tr>
+    `;
+    let totalCarrito = carrito.reduce((acumulador, prod) => acumulador + prod.precio, 0);
+    document.getElementById("totalPagar").innerText = `El total de tu compra es: $ ${totalCarrito * 1.21} con iva incluido`;
+}
+
+btnFC.onclick = () => {
+    carrito = [];
+    document.getElementById("tBody").innerHTML = "";
+    document.getElementById("totalPagar").innerText = "";
+    localStorage.clear();
+    sessionStorage.clear();
+    console.clear();
+    Swal.fire({
+        imageAlt: btnFC.nombre,
+        title: `Gracias por tu compra!`,
+        text: `En breve nos comunicaremos contigo`,
+        width: 400,
+        showConfirmButton: false,
+        timer: 3000,
+        background: `#f0e8e8`,
+        ///color: ``,
+        showClass: {
+            popup: "animate__animated animate__flip",
+        },
+        hideClass: {
+            popup: "animate__animated animate__fadeOutTopRight",
+        },
+    });
+};
