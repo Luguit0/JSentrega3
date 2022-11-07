877173f81bd7cced58b10c18b4352c9884966477
Claudio Lugo <claudiomlugo@gmail.com>
2022-11-07 09:01:03 -0300

    agregando mas productos

---
+++ js/jsarticulos2.js
@@ -0,0 +1,32 @@
+const articulos = [
+    {
+        id: 1,
+        nombre: `stanley\nblack`,
+        precio: 40000,
+        imagen: `botellapink`,
+    },
+    {
+        id: 2,
+        nombre: `stanley\nred`,
+        precio: 35000,
+        imagen: ``,
+    },
+    {
+        id: 3,
+        nombre: `stanley\nwhite`,
+        precio: 30000,
+        imagen: ``,
+    },
+    {
+        id: 4,
+        nombre: `stanley\npink`,
+        precio: 43000,
+        imagen: ``,
+    },
+    {
+        id: 5,
+        nombre: `combo\nstanley`,
+        precio: 90000,
+        imagen: ``,
+    }
+];
